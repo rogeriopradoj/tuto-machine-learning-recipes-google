5bbe81b6883341a1d7535a9b23cc5afe840ed254
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2016-05-31 05:22:02 -0300

    Create the second example from video 1 (car types)

--- hello-world.py
+++ hello-world.py
@@ -10,3 +10,14 @@ labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 print(clf.predict([[160, 0]]))
+
+horsepower = [300, 450, 200, 150]
+seats = [2, 2, 8, 9]
+features = [[h, s] for h, s in zip(horsepower, seats)]
+labels = [0, 0, 1, 1]
+# 0 sports-car
+# 1 minivan
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[300, 15]]))
