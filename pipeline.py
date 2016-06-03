7986291d60aa8f859be2068f74342be814972104
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2016-06-03 00:20:34 -0300

    Create examples from video 4 (dogs)

---
+++ pipeline.py
@@ -0,0 +1,33 @@
+# import a dataset
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
+
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predicitions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predicitions))
+
+
+## k neighbors
+
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predicitions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predicitions))
+
